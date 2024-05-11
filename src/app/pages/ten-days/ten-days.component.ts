dda255436e4e3fa8e8f1cc9ea0239d71e6cab362
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2024-05-11 14:54:07 +0100

    Make cards & create weather days interface

--- src/app/pages/ten-days/ten-days.component.ts
+++ src/app/pages/ten-days/ten-days.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeatherDays } from '../../interfaces/weather-days.interface';
 
 @Component({
   selector: 'app-ten-days',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './ten-days.component.css'
 })
 export class TenDaysComponent {
-
+  weatherList: WeatherDays[] = [
+    { date: '12 May', temperature: '28 ºC', humidity: '65 %', windSpeed: '25 Km/h' },
+    { date: '13 May', temperature: '30 ºC', humidity: '60 %', windSpeed: '20 Km/h' },
+    { date: '12 May', temperature: '28 ºC', humidity: '65 %', windSpeed: '25 Km/h' },
+    { date: '13 May', temperature: '30 ºC', humidity: '60 %', windSpeed: '20 Km/h' },
+    { date: '12 May', temperature: '28 ºC', humidity: '65 %', windSpeed: '25 Km/h' },
+    { date: '13 May', temperature: '30 ºC', humidity: '60 %', windSpeed: '20 Km/h' },
+    { date: '12 May', temperature: '28 ºC', humidity: '65 %', windSpeed: '25 Km/h' },
+    { date: '13 May', temperature: '30 ºC', humidity: '60 %', windSpeed: '20 Km/h' },
+    { date: '12 May', temperature: '28 ºC', humidity: '65 %', windSpeed: '25 Km/h' },
+    { date: '13 May', temperature: '30 ºC', humidity: '60 %', windSpeed: '20 Km/h' },
+  ];
 }
