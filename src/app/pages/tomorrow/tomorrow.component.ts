20f64e27aae46f8237af30c9c2156396b1c3a6e7
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2024-05-11 13:26:56 +0100

    Finished tomorrow component

--- src/app/pages/tomorrow/tomorrow.component.ts
+++ src/app/pages/tomorrow/tomorrow.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Weather } from '../../interfaces/weather.interface';
 
 @Component({
   selector: 'app-tomorrow',
   templateUrl: './tomorrow.component.html',
   styleUrl: './tomorrow.component.css'
 })
-export class TomorrowComponent {
+export class TomorrowComponent implements OnInit {
 
+  currentDate = new Date();
+  tomorrowDate = new Date(this.currentDate);
+  daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  weathertomorrow: Weather[] = [];
+
+  constructor() {
+    this.tomorrowDate.setDate(this.currentDate.getDate() + 1);
+  }
+
+  ngOnInit() {
+    this.weathertomorrow.push({
+      temperature: 30,
+      humidity: 65,
+      windSpeed: 25,
+      date: {
+        dayOfWeek: this.daysOfWeek[this.tomorrowDate.getDay()],
+        fullDate: `${this.tomorrowDate.getDate()} ${this.months[this.tomorrowDate.getMonth()]}`,
+        year: this.tomorrowDate.getFullYear(),
+        time: this.tomorrowDate.toLocaleTimeString(),
+      },
+    });
+  }
 }
+
